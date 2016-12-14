7d7d4b8e715cb7049563bb7b6823f12baed40c37
kcklight <kaiserho.kck@gmail.com>
2016-12-14 05:22:03 +0000

    static navbar

--- web/views.py
+++ web/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'web/index.html')
+    
+def products(request):
+    return render(request, 'web/products.html')
